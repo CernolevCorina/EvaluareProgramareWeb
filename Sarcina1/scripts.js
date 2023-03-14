002e3095aee0d363d01ab33f49101d0d420213dd
CernolevCorina <corina.cernolev@powerit.md>
2023-03-14 08:04:57 +0200

    Sarcina 1

---
+++ Sarcina1/scripts.js
@@ -0,0 +1,15 @@
+const number = Number(prompt('Please enter a number from 0 to 9: '));
+
+switch (number) {
+  case 0: alert('zero'); break;
+  case 1: alert('unu'); break;
+  case 2: alert('doi'); break;
+  case 3: alert('trei'); break;
+  case 4: alert('patru'); break;
+  case 5: alert('cinci'); break;
+  case 6: alert('sase'); break;
+  case 7: alert('sapte'); break;
+  case 8: alert('opt'); break;
+  case 9: alert('noua'); break;
+  default: alert('Entered number is not from 0 to 9');
+}
